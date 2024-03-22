cb9ba06bc8a2d245af66a73e3d5fee968cad66b5
akashab99 <96919428+akashab99@users.noreply.github.com>
2024-03-22 09:59:10 +0530

    Adding the Base files and Corrections (#1)
    * Adding solution
    
    * new Changes and Corrections

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# security_header
+# Security Header Application & Directory Listing
+
+## Overview
+This application provides security headers to enhance the security posture of web applications and includes a directory listing feature for managing files and directories within a web server environment.
+
+## Features
+1. **Security Headers**: Automatically injects security headers into HTTP responses to protect against common web vulnerabilities such as Cross-Site Scripting (XSS), Clickjacking, Content-Type sniffing, and more.
+   
+2. **Directory Listing**: Provides a user-friendly interface for browsing files and directories within a specified root directory on a web server. This feature enhances file management and navigation capabilities.
+
+
+## Installation:
+- ### Environment:
+- Create an environment.
+  - python -m venv env.
+- Actiavte the environment.
+  - "env/Scripts/activate".
+- Go to the App directory.
+
+## Requirements
+- Install the required packages.
+  - pip install -r requirements.txt
+
+## Run the Application
+- flask run --port=9000
---
+++ app/server/static/scripts/scan.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    var textarea1 = document.getElementById("sechead");
+    var textarea2 = document.getElementById("missing_sec");
+    var textarea3 = document.getElementById("listing");
+    $("#scanText").click(function(){
+        $(".result_value").addClass("common-none")
+        textarea1.value= ""
+        textarea2.value= ""
+        textarea3.value = ""
+        var url_entered = $('#textInput').val();
+        console.log(url_entered)
+        $.ajax({
+            url: "/url_scan",
+            type: "POST",
+            data: {value: url_entered},
+            success: function(response) {
+                var value1 = response['data_value1']
+                var value2 = response['data_value2']
+                var value3 = response['data_value3']
+                for (var header in value1)
+                 {
+                    textarea1.value += `${header}: ${value1[header]}` + "\n"
+                }
+
+                for (var item1 of value2)
+                    {
+                       textarea2.value += item1 + "\n"
+                    }
+                for (var item2 of value3)
+                    {
+                       textarea3.value += item2 + "\n"
+                    }
+                $(".result_value").removeClass("common-none")
+
+            },
+            error: function(xhr, status, error) {
+                // Handle error response
+                console.error(xhr.responseText);
+            }
+        });
+    });
+});
